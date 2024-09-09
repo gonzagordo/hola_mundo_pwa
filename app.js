3ae4b724a13c2a97203a2bbe7cb1533720638e48
gonzagordo <agustingonzaga77@gmail.com>
2024-09-09 11:49:33 +0200

    primera version funcional, con una unica funcion enciende luz comedor tomaaaaaaa

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/hola_mundo_pwa/sw.js').then(registration => {
+        navigator.serviceWorker.register('./sw.js').then(registration => {
             console.log('ServiceWorker registrado con éxito:', registration);
         }).catch(error => {
             console.log('Error en el registro del ServiceWorker:', error);
@@ -15,11 +15,12 @@ if ('serviceWorker' in navigator) {
 
 document.getElementById('pulsador_perimetro').addEventListener('click', function() {
     // URL de la IP local
-    const url = 'http://192.168.8.104/APAGA=PERIMETRO';  // 
+    const url = 'http://192.168.8.104/?encender+zona+=comedor+';  // 
 
     // Envío de una solicitud POST a la IP local sin esperar respuesta
     fetch(url, {
-        method: 'POST'
+        method: 'GET',
+        mode: 'no-cors'
     })
     .catch(error => console.error('Error al enviar la solicitud:', error));
 });
